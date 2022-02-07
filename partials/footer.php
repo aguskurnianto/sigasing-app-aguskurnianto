7b91665c8366614610e5ba18b7748bc550a99595
aguskurnianto <gus.kurnianto@gmail.com>
2022-02-07 09:26:26 +0800

    merapikan footer nav dan sidebar

--- partials/footer.php
+++ partials/footer.php
@@ -1,9 +1,7 @@
- <!-- Main Footer -->
- <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      Anything you want
+<!-- Main Footer -->
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b> version </b> 0.1.0
     </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2022 <a href="#">Praktikum Web </a>.</strong> All rights reserved.
   </footer>
